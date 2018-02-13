c193dd527278a96dff04dd6c88ac954b4f9109c2
MarcoDellOlio <dellolio.marco@gmail.com>
2018-02-13 16:37:34 -0500

    react can retrieve list of cities

--- client/src/App.js
+++ client/src/App.js
@@ -10,104 +10,12 @@ import axios from 'axios'
 
 class App extends Component {
   state = {
-    cities: [
-      {
-        id: 1,
-        name: 'Atlanta',
-        picture: 'http://www.unit2fitness.com/wp-content/uploads/2013/01/Graffiti-Wallpaper-027.jp' +
-            'g'
-      }, {
-        id: 2,
-        name: 'Tokyo',
-        picture: 'https://images.unsplash.com/photo-1493780474015-ba834fd0ce2f?ixlib=rb-0.3.5&ixid' +
-            '=eyJhcHBfaWQiOjEyMDd9&s=5f393936aef538204adbdb152665340c&auto=format&fit=crop&w=' +
-            '2226&q=80'
-      }, {
-        id: 3,
-        name: 'London',
-        picture: 'https://images.unsplash.com/photo-1498829356886-bd4efcaf0901?ixlib=rb-0.3.5&ixid' +
-            '=eyJhcHBfaWQiOjEyMDd9&s=fe8ff86b4c93e92d8a959804bf2b9901&auto=format&fit=crop&w=' +
-            '2550&q=80'
-      }, {
-        id: 4,
-        name: 'Atlanta',
-        picture: 'http://www.unit2fitness.com/wp-content/uploads/2013/01/Graffiti-Wallpaper-027.jp' +
-            'g'
-      }, {
-        id: 5,
-        name: 'Tokyo',
-        picture: 'https://images.unsplash.com/photo-1493780474015-ba834fd0ce2f?ixlib=rb-0.3.5&ixid' +
-            '=eyJhcHBfaWQiOjEyMDd9&s=5f393936aef538204adbdb152665340c&auto=format&fit=crop&w=' +
-            '2226&q=80'
-      }, {
-        id: 6,
-        name: 'London',
-        picture: 'https://images.unsplash.com/photo-1498829356886-bd4efcaf0901?ixlib=rb-0.3.5&ixid' +
-            '=eyJhcHBfaWQiOjEyMDd9&s=fe8ff86b4c93e92d8a959804bf2b9901&auto=format&fit=crop&w=' +
-            '2550&q=80'
-      }, {
-        id: 7,
-        name: 'Atlanta',
-        picture: 'http://www.unit2fitness.com/wp-content/uploads/2013/01/Graffiti-Wallpaper-027.jp' +
-            'g'
-      }, {
-        id: 8,
-        name: 'Tokyo',
-        picture: 'https://images.unsplash.com/photo-1493780474015-ba834fd0ce2f?ixlib=rb-0.3.5&ixid' +
-            '=eyJhcHBfaWQiOjEyMDd9&s=5f393936aef538204adbdb152665340c&auto=format&fit=crop&w=' +
-            '2226&q=80'
-      }, {
-        id: 9,
-        name: 'London',
-        picture: 'https://images.unsplash.com/photo-1498829356886-bd4efcaf0901?ixlib=rb-0.3.5&ixid' +
-            '=eyJhcHBfaWQiOjEyMDd9&s=fe8ff86b4c93e92d8a959804bf2b9901&auto=format&fit=crop&w=' +
-            '2550&q=80'
-      }, {
-        id: 10,
-        name: 'Atlanta',
-        picture: 'http://www.unit2fitness.com/wp-content/uploads/2013/01/Graffiti-Wallpaper-027.jp' +
-            'g'
-      }, {
-        id: 11,
-        name: 'Tokyo',
-        picture: 'https://images.unsplash.com/photo-1493780474015-ba834fd0ce2f?ixlib=rb-0.3.5&ixid' +
-            '=eyJhcHBfaWQiOjEyMDd9&s=5f393936aef538204adbdb152665340c&auto=format&fit=crop&w=' +
-            '2226&q=80'
-      }, {
-        id: 12,
-        name: 'London',
-        picture: 'https://images.unsplash.com/photo-1498829356886-bd4efcaf0901?ixlib=rb-0.3.5&ixid' +
-            '=eyJhcHBfaWQiOjEyMDd9&s=fe8ff86b4c93e92d8a959804bf2b9901&auto=format&fit=crop&w=' +
-            '2550&q=80'
-      }, {
-        id: 13,
-        name: 'Atlanta',
-        picture: 'http://www.unit2fitness.com/wp-content/uploads/2013/01/Graffiti-Wallpaper-027.jp' +
-            'g'
-      }, {
-        id: 14,
-        name: 'Tokyo',
-        picture: 'https://images.unsplash.com/photo-1493780474015-ba834fd0ce2f?ixlib=rb-0.3.5&ixid' +
-            '=eyJhcHBfaWQiOjEyMDd9&s=5f393936aef538204adbdb152665340c&auto=format&fit=crop&w=' +
-            '2226&q=80'
-      }, {
-        id: 15,
-        name: 'London',
-        picture: 'https://images.unsplash.com/photo-1498829356886-bd4efcaf0901?ixlib=rb-0.3.5&ixid' +
-            '=eyJhcHBfaWQiOjEyMDd9&s=fe8ff86b4c93e92d8a959804bf2b9901&auto=format&fit=crop&w=' +
-            '2550&q=80'
-      }, {
-        id: 13,
-        name: 'Atlanta',
-        picture: 'http://www.unit2fitness.com/wp-content/uploads/2013/01/Graffiti-Wallpaper-027.jp' +
-            'g'
-      }
-    ]
+    cities: []
   }
 
   getCities = () => {
     axios.get('/api/cities')
-    .then((res) => {console.log(res)})
+    .then((res) => {this.setState({cities : res.data}) })
   }
 
   componentWillMount() {
