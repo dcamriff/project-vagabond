d935b599715f9fbe486c2b07139d48dae07ff969
MarcoDellOlio <dellolio.marco@gmail.com>
2018-02-14 17:13:34 -0500

    added login logic

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -13,14 +13,20 @@ class Api::UsersController < ApplicationController
     render json: @user
   end
 
+
   # POST /users
   def create
-    @user = User.new(user_params)
-
-    if @user.save
-      render json: @user, status: :created, location: @user
-    else
-      render json: @user.errors, status: :unprocessable_entity
+  
+  if user_params[:firstname] == nil
+          @existing_user = User.where(username: user_params[:username])
+          render json: @existing_user
+    else 
+          @user = User.new(user_params)
+          if @user.save
+            render json: @user, status: :created, location: @user
+          else
+            render json: @user.errors, status: :unprocessable_entity
+          end
     end
   end
 
