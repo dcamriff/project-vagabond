135b598560700b7f848c85a4669656b94c9e51bf
MarcoDellOlio <dellolio.marco@gmail.com>
2018-02-14 11:57:20 -0500

    added login logic

--- client/src/components/LoginForm.js
+++ client/src/components/LoginForm.js
@@ -1,5 +1,25 @@
 import React from 'react'
 
+state = {
+    username: ""
+  }
+
+  handlChange = (event) => {
+    this.setState({ [event.target.name]: event.target.value })
+    event.preventDefault()
+  }
+
+  handleSubmit = (event) => {
+    axios.post('/api/users', this.state)
+      .then((res) => {
+        this.props.updateState(res.data)
+        localStorage.setItem("userId", res.data._id)
+        this.setState(res.data)
+      })
+      .catch((error) => { console.log(error) })
+    event.preventDefault()
+  }
+
 const LoginForm = () => {
     return (
         <div>
